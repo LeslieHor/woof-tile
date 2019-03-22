cc66d57ecd286a3aa42289b20b1039e8b555814c
leslie <leslie.hor@gmail.com>
2019-03-22 19:49:59 +0000

    command descriptions

--- lib/src/enums.py
+++ lib/src/enums.py
@@ -57,35 +57,83 @@ class OPTIONS:
     RENAME_SCREEN = 'rs'
 
 
-def print_options(windows):
+def get_option_description(option):
+    if option == OPTIONS.NONE:
+        return ''
+    elif option == OPTIONS.DEBUG:
+        return '(debug) print out debug information'
+    elif option == OPTIONS.RESTORE:
+        return '(restore) restore all window positions'
+    elif option == OPTIONS.LIST:
+        return '(list) TODO'
+    elif option == OPTIONS.ADD_HORIZONTAL:
+        return '--> (add horizontal) add the active window the the right of a target window'
+    elif option == OPTIONS.ADD_VERTICAL:
+        return '--> (add vertical) add the active window below the target window'
+    elif option == OPTIONS.EXPAND_HORIZONTAL:
+        return '(expand horizontal) increase the horizontal size of the active window'
+    elif option == OPTIONS.REDUCE_HORIZONTAL:
+        return '(reduce horizontal) decrease the horizontal size of the active window'
+    elif option == OPTIONS.EXPAND_VERTICAL:
+        return '(expand vertical) increase the vertical size of the active window'
+    elif option == OPTIONS.REDUCE_VERTICAL:
+        return '(reduce vertical) decrease the vertical size of the active window'
+    elif option == OPTIONS.CHANGE_PLANE:
+        return '(change plane) change a vertical split to a horizontal split and vice versa'
+    elif option == OPTIONS.SWAP_PANE:
+        return '(swap-pane) swap the two windows in the split'
+    elif option == OPTIONS.SWAP:
+        return '--> (swap) swap the positions of two windows in the tree'
+    elif option == OPTIONS.MINIMIZE_ALL:
+        return '(minimize all) minimize all windows in woof'
+    elif option == OPTIONS.UNMINIMIZE_ALL:
+        return '(unminimize all) unminimize all windows in woof'
+    elif option == OPTIONS.MAXIMIZE:
+        return '(maximize) maximize the active window as if it was the only window on the screen'
+    elif option == OPTIONS.KILL:
+        return '(kill) attempt to close the window and remove it from woof'
+    elif option == OPTIONS.REMOVE:
+        return '(remove) remove the window from woof'
+    elif option == OPTIONS.MOVE_TO:
+        return '(move to) move the active window to another window as a split'
+    elif option == OPTIONS.NAV_RIGHT:
+        return '(nav right) focus window the the right'
+    elif option == OPTIONS.NAV_LEFT:
+        return '(nav left) focus window to the left'
+    elif option == OPTIONS.NAV_UP:
+        return '(nav up) focus window to the top'
+    elif option == OPTIONS.NAV_DOWN:
+        return '(nav down) focus window to the bottom'
+    elif option == OPTIONS.ADD_TO_GROUP:
+        return '--> (add to group) add window as a group'
+    elif option == OPTIONS.ACTIVATE_NEXT_WINDOW_IN_GROUP:
+        return '(next window in group) activate next window in group'
+    elif option == OPTIONS.ACTIVATE_PREV_WINDOW_IN_GROUP:
+        return '(prev window in group) activate prev window in group'
+    elif option == OPTIONS.SWAP_SCREENS:
+        return '--> (swap screen) swap the two screens'
+    elif option == OPTIONS.NEW_SCREEN:
+        return '(new screen) adds a new screen (not functional)'
+    elif option == OPTIONS.LIST_SCREENS:
+        return '(list screens) list all the screens in woof'
+    elif option == OPTIONS.RENAME_SCREEN:
+        return '(rename screen) rename the current screen'
+
+    else:
+        return ''
+
+
+def print_options(_windows):
     exclude = ['__module__', '__doc__', 'NONE']
     commands = []
     for option, command in vars(OPTIONS).items():
         if option in exclude:
             continue
-        commands.append(command)
-    # commands += create_specials(windows)
+        listing = command
+        listing += " : " + get_option_description(command)
+        commands.append(listing)
     commands.sort()
 
     for command in commands:
         print(command)
 
-
-def create_specials(windows):
-    specials = []
-    windows_list = windows.list_add_windows()
-    specials += prepend_command(OPTIONS.ADD_HORIZONTAL, windows_list)
-    specials += prepend_command(OPTIONS.ADD_VERTICAL, windows_list)
-    specials += prepend_command(OPTIONS.SWAP, windows_list)
-    specials += prepend_command(OPTIONS.MOVE_TO, windows_list)
-    specials += prepend_command(OPTIONS.ADD_TO_GROUP, windows_list)
-    specials += windows.list_screens(OPTIONS.SWAP_SCREENS, True)
-
-    return specials
-
-
-def prepend_command(prepend, window_list):
-    new_window_list = []
-    for print_out in window_list:
-        new_window_list.append(prepend + print_out)
-    return new_window_list
